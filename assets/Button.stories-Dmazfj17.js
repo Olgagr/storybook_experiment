d3d64c420bc8a463c798b49a4edb50a6049af131
olgagr <olga.grabek@gmail.com>
2024-08-23 13:04:06 +0000

    deploy: 95f997d5c8e8a21324b3137d60b78048e5ab1e47

--- assets/Button.stories-Dmazfj17.js
+++ assets/Button.stories-Dmazfj17.js
@@ -1,4 +1,4 @@
-import{B as b,f as y}from"./Button-DmlixO25.js";import"./index-DZLKizrv.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const k={title:"Example/Button",component:b,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:y()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+import{B as b,f as y}from"./Button-Dc1hiOwI.js";import"./index-DZLKizrv.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const k={title:"Example/Button",component:b,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:y()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}};var s,t,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
   args: {
     primary: true,
     label: "Button"
